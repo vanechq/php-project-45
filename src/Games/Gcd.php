c5e162126578bd9198493b0a85d7ec550add8e16
vanechq <vanechq@gmail.com>
2022-09-04 14:20:20 +1000

    changed Gcd

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -10,7 +10,7 @@ function gcd(int $a, int $b): int
     do {
         $a > $b ? $a %= $b : $b %= $a;
         $result = $a + $b;
-    } while ($a !== 0 && $b !== 0);
+    } while ($a != 0 && $b != 0);
     return $result;
 }
 
@@ -21,8 +21,8 @@ function play(): void
     line('Hello, %s!', $name);
     line("Find the greatest common divisor of given numbers.");
     for ($i = 1; $i <= 3; $i++) {
-        $a = rand(0, 100);
-        $b = rand(0, 100);
+        $a = rand(1, 100);
+        $b = rand(1, 100);
         $result = gcd($a, $b);
         line("Question: $a $b");
         $answer = prompt('Your answer');
